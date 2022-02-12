5e1668aebb78ab9e577b8c59d49392ee518a599e
Chanakyamaradana <99252943+Chanakyamaradana@users.noreply.github.com>
2022-02-12 21:36:47 +0530

    Create a.s.d.f

---
+++ a.s.d.f
@@ -0,0 +1,15 @@
+INPUT:
+a=int(input('enter a value:'))
+b=int(input('enter b value:'))
+if(a==b):
+   print('equal')
+else:
+     if a>b:
+         print('not equal a is big')
+     else:
+         print('not equal b is big')
+         
+    Output:
+enter a value:25
+enter b value:49
+not equal b is big     
